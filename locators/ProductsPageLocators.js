cc74b59276529b6e09de5d2aa42301d4cc4c2c72
j81288423-web <j81288423@gmail.com>
2026-08-28 22:26:15 +0530

    Delete locators/ProductsPageLocators.js

--- locators/ProductsPageLocators.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  productsLink: (page) => page.getByRole('link', { name: 'Products' }),
-  searchInput: (page) => page.locator('#search_product'),
-  searchButton: (page) => page.locator('#submit_search'),
-  searchedProductsHeading: (page) => page.getByText('Searched Products', { exact: false }),
-  matchingProducts: (page) => page.locator('.productinfo p').filter({ hasText: /T SHIRT/i })
-};
